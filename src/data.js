a7dea7317495df94e5adf1d93dbf49ebc2009c49
Brandon Wirz <bwirz75@gmail.com>
2022-12-12 00:03:19 -0700

    adjust flex on  mobile styles

--- src/data.js
+++ src/data.js
@@ -18,7 +18,7 @@ const data = [
     startDate: "12 Jan, 2022",
     endDate: "24 Jan, 2022",
     description:
-      "Matterhorn,one o,g the frontier between Switzerland and Italy, 6 miles (10 km) southwest of the village of Zermatt, Switzerland. Matterhorn means “the peak in the meadows,” from the German words Matte (“meadow”)",
+      "Matterhorn, between Switzerland and Italy, southwest of the village of Zermatt, Switzerland. Matterhorn means “the peak in the meadows,” From the German word Matte (“meadow”)",
     imageUrl: "matterhorn.jpg",
   },
   {
@@ -29,7 +29,7 @@ const data = [
     startDate: "12 Jan, 2022",
     endDate: "24 Jan, 2022",
     description:
-      "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+      "Whistler Blackcomb is a ski resort located in Whistler, British Columbia, Canada. The largest ski resort in North America with the greatest lift capacity. Whistler and Blackcomb mountains",
     imageUrl: "whistler-blackcomb.jpeg",
   },
 ]
